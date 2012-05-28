224abebf86a990b080f8209aed918686575fa507
simsicon <simsicon@gmail.com>
2012-05-28 18:13:48 +0800

    fix

--- db/migrate/20120528095134_add_two_basic_commands_into_db.rb
+++ db/migrate/20120528095134_add_two_basic_commands_into_db.rb
@@ -1,10 +1,8 @@
 class AddTwoBasicCommandsIntoDb < ActiveRecord::Migration
   def up  
-    Action.all.each do |action|
-      unless action.name == 'write' || action.name == 'reboot'
-        Action.create(:name => 'write', :description => 'write to white list', :command => "echo 'ARG' >> /jffs/openvpn/vpnup_custom")
-        Action.create(:name => 'reboot', :description => 'reboot the router', :command => "reboot")
-      end
+    unless Action.find_by_name('write') || Action.find_by_name('reboot')
+      Action.create(:name => 'write', :description => 'write to white list', :command => "echo 'ARG' >> /jffs/openvpn/vpnup_custom")
+      Action.create(:name => 'reboot', :description => 'reboot the router', :command => "reboot")
     end
   end
 
